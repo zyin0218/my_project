918c8012a49c99f85bac6f68cc0e5a1be1f3c093
zyin <zyin0218@gmail.com>
2022-05-08 14:08:12 +0800

    add pages, products controller, and product model

---
+++ db/migrate/20220508054046_create_products.rb
@@ -0,0 +1,12 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.integer :price
+      t.text :descrtption
+      t.integer :sku
+
+      t.timestamps
+    end
+  end
+end
