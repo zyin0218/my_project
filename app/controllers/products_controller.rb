7b7a269ffb01b48faaa67c87bbace4478899da9b
zyin <zyin0218@gmail.com>
2022-05-08 17:00:56 +0800

    新增products new, index, show頁面

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,37 @@
 class ProductsController < ApplicationController
+  
+  def index
+    @products = Product.all
+  end
+
   def new
+    @products = Product.new
+  end
+
+  def create
+    @products = Product.new(product_params)
+    if @products.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @products = Product.find(params["id"])
+  end
+
+  def destroy
+    @products = Product.find(params["id"])
+    @products.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :title, :price, :sku, :description
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "pages#index"
+  # root "pages#index"
+  root "products#index"
   get "products/new", to: "products#new", as: "new_product"
+  get "products", to: "products#index"
+  post "products", to: "products#create"
+  get "products/:id", to: "products#show", as: "product"
+  delete "products/:id", to: "products#destroy"
 end
